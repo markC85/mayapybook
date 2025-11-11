f8e4c3ef1f03ffe94eaab83f8050c172202be1ac
Mark-Conrad <mark.conrad@2k.com>
2025-11-11 23:52:02 +0000

    Updated with info function

--- pylib/minspect.py
+++ pylib/minspect.py
@@ -1,8 +1,33 @@
 import sys
 import pymel.core as pmc
+from pprint import pprint
+
 
 def syspath():
-    """Return the current Python sys.path as a list of strings."""
+    """
+    Return the current Python system paths as a list of strings
+    """
+    system_paths = sorted([p.replace("\\", "/") for p in sys.path if p])
     print("sys.paths Found:")
-    for p in sys.path:
-        print(f"  {p}")
+    for path in system_paths:
+        print(f'    {path}')
+
+
+def info(obj):
+    """
+    Prints information about the object
+
+    Args:
+        obj: python object to inspect
+    """
+    lines = [f'Info for {obj.name()}',
+             'Attributes:']
+    # Get the name of all attributes
+    for a in obj.listAttr():
+        lines.append(f'  {a.name()}')
+
+    lines.append(f'MEL type: {obj.type()}')
+    lines.append('MRO:')
+    lines.extend([' ' + t.__name__ for t in type(obj).__mro__])
+    result = '\n'.join(lines)
+    print(result)
